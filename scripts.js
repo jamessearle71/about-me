37f5d3a3d52402afed5da060715684e0f632f12f
James Searle <jamessearle41@hotmail.co.uk>
2023-04-03 10:37:30 +0100

    resolved js error on line 193

--- scripts.js
+++ scripts.js
@@ -46,60 +46,63 @@ let userScore = 0;
 
 alert("You have " + userScore + " points to start.");
 
-function questionOne(){
-let student = prompt(
-  "Is James a student? (Please answer either yes/no or y/n)"
-);
-if (student.toLowerCase() === "yes" || student.toLowerCase() === "y") {
-  //  console.log("You're correct! James is currently a student.");
-  userScore++;
-  alert("You're correct! James is currently a student.");
-} else {
-  // console.log("Too bad! That's incorrect.");
-  alert("Too bad! That's incorrect.");
-}
+function questionOne() {
+  let student = prompt(
+    "Is James a student? (Please answer either yes/no or y/n)"
+  );
+  if (student.toLowerCase() === "yes" || student.toLowerCase() === "y") {
+    //  console.log("You're correct! James is currently a student.");
+    userScore++;
+    alert("You're correct! James is currently a student.");
+  } else {
+    // console.log("Too bad! That's incorrect.");
+    alert("Too bad! That's incorrect.");
+  }
 }
 questionOne();
 
-function questionTwo(){
-let amphibian = prompt(
-  "Does James work with slimy creatures in his spare time? (Please answer either yes/no or y/n)"
-);
-if (amphibian.toLowerCase() === "yes" || amphibian.toLowerCase() === "y") {
-  //  console.log("That's right! James spends way too much time thinking about toads.");
-  userScore++;
-  alert("That's right! James spends way too much time thinking about toads.");
-} else {
-  // console.log("Not the greatest start...");
-  alert("Not the greatest start...");
-}
+function questionTwo() {
+  let amphibian = prompt(
+    "Does James work with slimy creatures in his spare time? (Please answer either yes/no or y/n)"
+  );
+  if (amphibian.toLowerCase() === "yes" || amphibian.toLowerCase() === "y") {
+    //  console.log("That's right! James spends way too much time thinking about toads.");
+    userScore++;
+    alert("That's right! James spends way too much time thinking about toads.");
+  } else {
+    // console.log("Not the greatest start...");
+    alert("Not the greatest start...");
+  }
 }
 questionTwo();
 
-
-function questionThree(){
-let retail = prompt("Is James interested in a career in retail? (Please answer either yes/no or y/n)");
-if (retail.toLowerCase() === "no" || retail.toLowerCase() === "n") {
-  //  console.log("Well done! Very impressive.");
-  userScore++;
-  alert("Well done! Very impressive.");
-} else {
-  // console.log("Unlucky! That's the wrong asnwer");
-  alert("Unlucky! That's the wrong answer.");
-}
+function questionThree() {
+  let retail = prompt(
+    "Is James interested in a career in retail? (Please answer either yes/no or y/n)"
+  );
+  if (retail.toLowerCase() === "no" || retail.toLowerCase() === "n") {
+    //  console.log("Well done! Very impressive.");
+    userScore++;
+    alert("Well done! Very impressive.");
+  } else {
+    // console.log("Unlucky! That's the wrong asnwer");
+    alert("Unlucky! That's the wrong answer.");
+  }
 }
 questionThree();
 
-function questionFour(){
-let coding = prompt("Is James currently learning to code? (Please answer either yes/no or y/n)");
-if (coding.toLowerCase() === "yes" || coding.toLowerCase() === "y") {
-  //  console.log("You're on a roll! Impressive.");
-  userScore++;
-  alert("Super! That's right!");
-} else {
-  // console.log("Unlucky! That's the wrong asnwer");
-  alert("You suck!");
-}
+function questionFour() {
+  let coding = prompt(
+    "Is James currently learning to code? (Please answer either yes/no or y/n)"
+  );
+  if (coding.toLowerCase() === "yes" || coding.toLowerCase() === "y") {
+    //  console.log("You're on a roll! Impressive.");
+    userScore++;
+    alert("Super! That's right!");
+  } else {
+    // console.log("Unlucky! That's the wrong asnwer");
+    alert("You suck!");
+  }
 }
 questionFour();
 
@@ -171,18 +174,20 @@ function guessSkill() {
 }
 guessSkill();
 
-function questionSeven(){
-let diploma = prompt(
-  "Final question: does James have a degree in Business Management? (Please answer either yes/no or y/n)"
-);
-if (diploma.toLowerCase() === "no" || diploma.toLowerCase() === "n") {
-  //  console.log("Woo! That's correct, thanks for completing the quiz!");
-  userScore++;
-  alert("Woo! That's correct.");
-} else {
-  // console.log("Better luck next time...");
-  alert("Better luck next time...");
-} alert(
-  "Thanks for completing the quiz! You have a score of " + userScore + "/7."
-);}
-function questionSeven();
+function questionSeven() {
+  let diploma = prompt(
+    "Final question: does James have a degree in Business Management? (Please answer either yes/no or y/n)"
+  );
+  if (diploma.toLowerCase() === "no" || diploma.toLowerCase() === "n") {
+    //  console.log("Woo! That's correct, thanks for completing the quiz!");
+    userScore++;
+    alert("Woo! That's correct.");
+  } else {
+    // console.log("Better luck next time...");
+    alert("Better luck next time...");
+  }
+  alert(
+    "Thanks for completing the quiz! You have a score of " + userScore + "/7."
+  );
+}
+questionSeven();
